4a22e9f5487de03c6cdd299c70341020194bb911
German <germanjosearielescobarescobar@gmai.com>
2026-04-10 08:37:17 -0600

    cuenta bancaria

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+class CuentaBancaria
+{
+    private double saldo;
+
+    public string Saldo
+    {
+        get
+        {
+            if (saldo > 0)
+                return saldo.ToString();
+            else
+                return "Saldo no disponible";
+        }
+    }
+
+    public void SetSaldo(double valor)
+    {
+        saldo = valor;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.SetSaldo(-50);
+
+        Console.WriteLine(cuenta.Saldo);
+    }
+}
