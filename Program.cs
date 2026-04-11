2b36b008cc208cd9faf2799b0b036409281e9bca
Martinez . <pm789481@gmail.com>
2026-04-11 12:50:50 -0600

    cuenta bancaria

--- Program.cs
+++ Program.cs
@@ -1,33 +1,30 @@
 ﻿using System;
+class Program
+{
+    static void Main()
+    {
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.SetSaldo(500);
+        Console.WriteLine(cuenta.Saldo);
+    }
+}
 
 class CuentaBancaria
 {
     private double saldo;
-
+    public void SetSaldo(double valor)
+    {
+        saldo = valor;
+    }
     public string Saldo
     {
         get
         {
             if (saldo > 0)
-                return saldo.ToString();
-            else
-                return "Saldo no disponible";
+            {
+                return saldo + "";
+            }
+            return "Saldo no disponible";
         }
-    }
-
-    public void SetSaldo(double valor)
-    {
-        saldo = valor;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        CuentaBancaria cuenta = new CuentaBancaria();
-        cuenta.SetSaldo(-50);
-
-        Console.WriteLine(cuenta.Saldo);
     }
 }
